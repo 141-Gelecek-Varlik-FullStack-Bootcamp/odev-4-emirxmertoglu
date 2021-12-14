4a682a5659b1a35ed84775053f7bc8af0882cc66
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-15 02:46:43 +0300

    added base controller file for api controllers

---
+++ Broot/Broot.API/Infrastructer/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Broot.API.Infrastructer
+{
+    public class BaseController : ControllerBase
+    {
+        private IMemoryCache memoryCache;
+
+        public BaseController(IMemoryCache _memoryCache)
+        {
+            memoryCache = _memoryCache;
+        }
+
+        public Broot.Model.UserModel.UserLoginModel CurrentUser { get { return GetCurrentUser(); } }
+
+        private Broot.Model.UserModel.UserLoginModel GetCurrentUser()
+        {
+            var response = new Broot.Model.UserModel.UserLoginModel();
+            memoryCache.TryGetValue(CacheKeys.Login, out response);
+
+            //if (memoryCache.TryGetValue(CacheKeys.Login, out Broot.Model.UserModel.UserLoginModel loginUser))
+            //{
+            //    response = loginUser;
+            //}
+
+            return response;
+        }
+    }
+}
