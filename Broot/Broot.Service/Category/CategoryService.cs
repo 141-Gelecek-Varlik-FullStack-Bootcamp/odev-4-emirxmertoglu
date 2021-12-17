d4f2ac7bc0faa6331a13a8cfbf58977741276959
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-18 00:21:13 +0300

    coded category service

---
+++ Broot/Broot.Service/Category/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Broot.DB.Entities.DataContext;
+using Broot.Model;
+using Broot.Model.CategoryModel;
+
+namespace Broot.Service.Category
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMapper mapper;
+
+        public CategoryService(IMapper _mapper)
+        {
+            mapper = _mapper;
+        }
+        // Insert Category
+        public General<CategoryDetail> Insert(InsertCategory newCategory)
+        {
+            var result = new General<CategoryDetail>() { IsSuccess = false };
+            try
+            {
+                var model = mapper.Map<Broot.DB.Entities.Category>(newCategory);
+                using (var srv = new BrootContext())
+                {
+                    model.Idate = System.DateTime.Now;
+                    model.IsActive = true;
+                    model.Iuser = 2;
+
+                    // Adding changes to db
+                    srv.Category.Add(model);
+                    srv.SaveChanges();
+
+                    // Returning category details to entity
+                    result.Entity = mapper.Map<CategoryDetail>(model);
+                    result.IsSuccess = true;
+                }
+            }
+            catch (System.Exception)
+            {
+                result.ExceptionMessage = "Beklenmeyen bir hata oluştu. Lütfen daha sonra tekrar deneyin!";
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ Broot/Broot.Service/Category/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Broot.Model;
+using Broot.Model.CategoryModel;
+
+namespace Broot.Service.Category
+{
+    public interface ICategoryService
+    {
+        // Insert category
+        public General<CategoryDetail> Insert(InsertCategory newCategory);
+
+    }
+}
