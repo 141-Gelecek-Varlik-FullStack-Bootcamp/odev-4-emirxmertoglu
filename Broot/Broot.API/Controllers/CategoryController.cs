c44087cced83d9218a366f3725d14500babe3daa
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-18 00:23:18 +0300

    added category controller with insert category

---
+++ Broot/Broot.API/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Broot.Model;
+using Broot.Service.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Broot.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+
+        [HttpPost]
+        public General<Model.CategoryModel.CategoryDetail> Insert([FromBody] Model.CategoryModel.InsertCategory newCategory)
+        {
+            General<Model.CategoryModel.CategoryDetail> response = new();
+
+            response = categoryService.Insert(newCategory);
+
+            return response;
+        }
+    }
+}
