44b095f8cfb8c931e328e4d23e119da92d16e375
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-18 00:22:24 +0300

    added category models

---
+++ Broot/Broot.Model/CategoryModel/CategoryDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace Broot.Model.CategoryModel
+{
+    public class CategoryDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Broot/Broot.Model/CategoryModel/InsertCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Broot.Model.CategoryModel
+{
+    public class InsertCategory
+    {
+        [Required(ErrorMessage ="Kategorinin temsili adini giriniz!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Kategorinin adini giriniz!")]
+        public string DisplayName { get; set; }
+    }
+}
