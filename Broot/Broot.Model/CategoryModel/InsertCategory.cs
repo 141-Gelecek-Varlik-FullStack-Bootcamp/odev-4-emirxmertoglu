17da879ea0db497216e002420db590262029a1cb
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-18 13:16:43 +0300

    coded max character validation

--- Broot/Broot.Model/CategoryModel/InsertCategory.cs
+++ Broot/Broot.Model/CategoryModel/InsertCategory.cs
@@ -5,8 +5,11 @@ namespace Broot.Model.CategoryModel
     public class InsertCategory
     {
         [Required(ErrorMessage ="Kategorinin temsili adini giriniz!")]
+        [MaxLength(50, ErrorMessage ="Kategorinin temsili adi max 50 karakter olabilir!")]
         public string Name { get; set; }
+
         [Required(ErrorMessage ="Kategorinin adini giriniz!")]
+        [MaxLength(50, ErrorMessage = "Kategorinin adi max 50 karakter olabilir!")]
         public string DisplayName { get; set; }
     }
 }
